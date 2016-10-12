86e016bf20be714f7b85c56d1c94fb67b40b6afe
Niclas Hjulström <niclas@hjulstrom.net>
2016-10-12 13:40:12 +0200

    Added Dog class. Finished part 1 of lab.

---
+++ OOP_Project1/Dog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace OOP_Project1
+{
+	public class Dog
+	{
+		public int Age { get; set; }
+		public string Breed { get; set; }
+		public string Name { get; set; }
+
+		public Dog(string name, string breed, int age)
+		{
+			this.Age = age;
+			this.Breed = breed;
+			this.Name = name;
+		}
+
+		public string GetDogInfo()
+		{
+			string ret =
+				"***** " + Name + " *****\n" + 
+				"Breed: " + Breed + "\n" +
+				"Age: " + Age + "\n" +
+				"******";
+			for (int i = 0; i < Name.Length; i++){
+				ret += "*";
+			}
+			ret += "******\n";
+			return ret;
+		}
+
+	}
+}
---
+++ OOP_Project1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OOP_Project1
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Dog dog1 = new Dog("Thea", "Labrador", 4);
+			Dog dog2 = new Dog("Tintin", "Miniature Schnauzer", 1);
+			Dog dog3 = new Dog("Sune", "chiwawa", 3);
+
+			Console.WriteLine(dog1.GetDogInfo());
+			Console.WriteLine(dog2.GetDogInfo());
+			Console.WriteLine(dog3.GetDogInfo());
+		}
+	}
+}
